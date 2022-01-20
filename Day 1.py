d3fd493ee19ce9d9dc2ebbed6df827284f5046a2
deepu14d <deepanshu2002d@gmail.com>
2022-01-20 17:12:29 +0530

    Day 1 Challenge Added

---
+++ Day 1.py
@@ -0,0 +1,14 @@
+""" This is Day 1 challenge in 30 days of code by HackerRank 
+	I am using Python3 to solve this and other upcoming challenges """
+
+""" Task - 
+To complete this challenge, you must save a line of input from stdin to a variable, print Hello, World. on a single line, and finally print the value of your variable on a second line. """
+
+# Read a full line of input and save it to our dynamically typed variable, input_string.
+input_string = input()
+
+# Print a string literal saying "Hello, World."
+print('Hello, World.')
+
+# TODO: Write a line of code here that prints the contents of input_string.
+print(input_string)
