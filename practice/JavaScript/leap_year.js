1e0009afef1c5b7c5ed82daec99a4f04d6471379
deepu14d <deepanshu2002d@gmail.com>
2022-04-26 12:40:09 +0530

    leap year checker

---
+++ practice/JavaScript/leap_year.js
@@ -0,0 +1,24 @@
+function isLeap(year){
+	var result;
+
+	if (year%4 === 0){
+		if (year%100 === 0){
+			if (year%400 === 0){
+				result = "Leap year";
+			}
+			else {
+				result = "Not leap year";
+			}
+		}
+		else {
+			result = "Leap year";
+		}
+	}
+	else {
+		result = "Not leap year";
+	}
+
+	return result;
+}
+
+isLeap(2000);
