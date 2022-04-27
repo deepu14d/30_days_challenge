f2180c28e09b6e3ba9b0171aa33fba5dc799cf45
deepu14d <deepanshu2002d@gmail.com>
2022-04-27 23:35:31 +0530

    fizzbuzz updated (loop)

--- practice/JavaScript/fizzbuzz.js
+++ practice/JavaScript/fizzbuzz.js
@@ -1,24 +1,23 @@
 var container = [];
 var count = 1;
 
-function fizzbuzz(){
-	if (count%5 === 0 && count%3 === 0){
-		container.push("fizzbuzz");
-		count++;
-	}
-	else if (count%3 === 0){
-		container.push("fizz");
-		count++;
-	}
 
-	else if (count%5 === 0){
-		container.push("buzz");
-		count++;
-	}
-	else {
-		container.push(count);
-		count++;
-	}
-	
-	console.log(container);
+function fizzbuzz(){
+		while (count <= 100){
+			if (count%5 === 0 && count%3 === 0){
+				container.push("fizzbuzz");
+			}
+			else if (count%3 === 0){
+				container.push("fizz");
+			}
+
+			else if (count%5 === 0){
+				container.push("buzz");
+			}
+			else {
+				container.push(count);
+			}
+			count++;
+			console.log(container);
+		}
 }
