c04a39682bde1cb99cd65ff08ce4333b19d440ff
deepu14d <deepanshu2002d@gmail.com>
2022-12-10 00:25:12 +0530

    a power b

---
+++ Java/Questions-Operators/src/Power.java
@@ -0,0 +1,17 @@
+// Calculate a power b (a^b) with complexity O(log(b))
+// using normal power and multiplication method Math.pow() then it takes complexity O(n)
+public class Power {
+    public static void main(String[] args) {
+        int base = 3;
+        int power = 6;
+        int ans = 1;
+        while(power > 0){
+            if((power&1)==1){
+                ans *= base;
+            }
+            base = base*base;
+            power = power >> 1;
+        }
+        System.out.println(ans);
+    }
+}
